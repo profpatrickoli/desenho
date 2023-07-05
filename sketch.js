730f623656c15b0e49b510ca10d1af6c9bbd5bb7
profpatrickoli <138583814+profpatrickoli@users.noreply.github.com>
2023-07-05 11:14:12 -0300

    Criei arquivo javascript

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+    background(0);
+
+}
+
+function draw() {
+  //background(0);
+  fill(0,255,0);
+  stroke(0,255,0);
+  if(mouseIsPressed)
+    circle(mouseX, mouseY, 20);  
+}
